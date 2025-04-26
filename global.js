e651e5a32c78738f394e086f1254eeb4d57c173d
annalyellis <141669562+annalyellis@users.noreply.github.com>
2025-04-25 19:48:34 -0700

    lab 4

--- global.js
+++ global.js
@@ -4,17 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// //2.1
-// navLinks = $$("nav a")
-
-// //2.2
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-
-// //2.3
-// currentLink.classList.add('current');
-// currentLink?.classList.add('current');
 
 let pages = [
   { url: '', title: 'Home' },
@@ -37,7 +26,6 @@ for (let p of pages) {
 
   url = !url.startsWith('http') ? BASE_PATH + url : url;
 
-  // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
@@ -90,4 +78,45 @@ select.addEventListener('input', function (event) {
 
   localStorage.colorScheme = newScheme;
 });
+
+
+// importing project data
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+    
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+
+// creating a renderProjects function
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  });
+
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
+
 
